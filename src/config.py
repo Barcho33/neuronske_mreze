9b77537bd5ed2a6b51d7ee510b8683dc1da0d768
Barcho33 <abaralic02@gmail.com>
2026-07-07 18:14:19 +0200

    chore: add configuration parameters

---
+++ src/config.py
@@ -0,0 +1,24 @@
+# Paths
+TRAIN_DIR = "../data/train"
+VAL_DIR = "../data/val"
+TEST_DIR = "../data/test"
+MODEL_SAVE_PATH = "../models/best_model.pth"
+
+# Dataset
+NUM_CLASSES = 8
+IMAGE_SIZE = 224
+BATCH_SIZE = 32
+NUM_WORKERS = 4
+
+# Training
+EPOCHS = 10
+LEARNING_RATE = 0.0001
+DEVICE = "cuda"
+
+# Model
+PRETRAINED = True
+FREEZE_BACKBONE = True
+MODEL_NAME = "resnet50"
+
+# Reproducibility
+SEED = 42
